a62aaf18c04d735decd77c75509fb9f091b36975
slinkstr <49608195+slinkstr@users.noreply.github.com>
2024-12-13 00:52:22 -0800

    Misc. cleanup

--- BeatleaderScoreScanner/Program.cs
+++ BeatleaderScoreScanner/Program.cs
@@ -10,39 +10,54 @@ internal class Program
     // to debug inconsistencies between program and bl
     private static Dictionary<long, long> BeatleaderUnderswings = new()
     {
+        // 1
+        { 19673962, 10136 },
+        { 19673722, 7764  },
+        { 19673336, 15036 },
+        { 19673256, 1520  },
+        { 19609190, 4470  },
+        { 19609148, 9484  },
+        { 19608960, 12902 },
+        { 19608876, 1624  },
+        { 19608834, 640   },
+        { 19608806, 2427  },
+        // 2
+        { 19544641, 15235 },
+        { 19544037, 44608 },
+        { 19543745, 1096  },
+        { 19543593, 6116  },
         { 19336178, 10186 },
         { 19336147, 538   },
         { 19296026, 1456  },
         { 19295964, 1156  },
         { 19295897, 2520  },
         { 19295716, 1328  },
+        // 3
         { 19295660, 1376  },
         { 19295620, 159   },
         { 19295391, 3162  },
         { 19257510, 23670 },
-
         { 19257484, 136   },
         { 19257455, 7680  },
         { 19257442, 396   },
         { 19257428, 1052  },
         { 19257378, 0     },
         { 19257344, 0     },
+        // 4
         { 19257322, 0     },
         { 19257304, 1856  },
         { 19257283, 256   },
         { 19257249, 0     },
-
         { 19257207, 112   },
         { 19257180, 96    },
         { 19257126, 0     },
-        { 18989377, 17526 },
         { 18989200, 7104  },
         { 18989122, 9832  },
         { 18989032, 648   },
+        // 5
         { 18988920, 9228  },
         { 18988866, 3160  },
         { 18988795, 5808  },
-
         { 18988598, 1312  },
         { 18875243, 12043 },
         { 18875207, 4820  },
@@ -50,10 +65,10 @@ internal class Program
         { 18875072, 410   },
         { 18834128, 13758 },
         { 18834087, 13112 },
+        // 6
         { 18834057, 7948  },
         { 18834001, 6992  },
         { 18833829, 13840 },
-
         { 18833658, 1490  },
         { 18833619, 1296  },
         { 18766658, 39793 },
@@ -61,10 +76,10 @@ internal class Program
         { 18766215, 2912  },
         { 18672659, 1664  },
         { 18672632, 4304  },
+        // 7
         { 18672521, 664   },
         { 18672508, 1444  },
         { 18672437, 406   },
-
         { 18672392, 537   },
         { 18630663, 8050  },
         { 18630638, 2440  },
@@ -72,10 +87,10 @@ internal class Program
         { 18630538, 2456  },
         { 18630471, 1612  },
         { 18630381, 5856  },
+        // 8
         { 18630328, 1368  },
         { 18630266, 664   },
         { 18630225, 309   },
-
         { 18514540, 1264  },
         { 18514378, 672   },
         { 18514241, 11705 },
@@ -83,10 +98,10 @@ internal class Program
         { 18514109, 21    },
         { 18379371, 19904 },
         { 18379249, 8740  },
+        // 9
         { 18379172, 2604  },
         { 18379044, 1088  },
         { 18378995, 5310  },
-
         { 18378936, 560   },
         { 18378812, 1976  },
         { 18378768, 1045  },
@@ -94,9 +109,17 @@ internal class Program
         { 18293991, 2364  },
         { 18293967, 1216  },
         { 18293876, 720   },
+        // 10
         { 18293808, 704   },
         { 18293534, 1288  },
         { 18293461, 1096  },
+        { 18293440, 858   },
+        { 18293392, 206   },
+        { 18144669, 3504  },
+        { 18144514, 4340  },
+        { 18144477, 464   },
+        { 18144363, 2400  },
+        { 18144324, 3600  },
     };
 
     /* easier to scrape pages with this
@@ -116,10 +139,10 @@ internal class Program
 
     let details = document.querySelectorAll("section.details");
     details.forEach((elm) => {
-	    elm.children[3].remove();
-	    elm.children[2].remove();
-	    elm.children[0].remove();
-	    });
+        elm.children[3].remove();
+        elm.children[2].remove();
+        elm.children[0].remove();
+        });
 
     })();
     */
@@ -234,17 +257,12 @@ internal class Program
     {
         var diff = score.leaderboard?.difficulty ?? score.difficulty ?? throw new Exception("Error parsing difficulty.");
 
-        // compare these against replay analysis
-        var scoreDate          = DateTimeOffset.FromUnixTimeSeconds((long)score.timeset).UtcDateTime;
-        var scoreBase          = (long)score.baseScore;
-        var scoreAcc           = (float)score.accuracy;
-        var scoreFc            = (bool)score.fullCombo;
-        var scoreMax           = (long)diff.maxScore;
-
         // required for linking leaderboards/replays
         var scoreId            = (string)score.id;
         var scoreLeaderboardId = (string)score.leaderboardId;
 
+        var scoreAcc           = (float)score.accuracy;
+        var scoreFc            = (bool)score.fullCombo;
         if(config.RequireFC && !scoreFc)   { return null; }
         if(config.MinimumScore > scoreAcc) { return null; }
 
